9632e02ff6e85020e2844b93134e73047dc76c60
Richard Antao <richardmantao@gmail.com>
2020-04-17 14:27:29 -0400

    Configured database

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 /* --- Configuration --- */
 const app = express();
 const port = process.env.PORT || 3001;
+require("./config/db");
 
 /* --- Middleware --- */
 app.use(helmet());
