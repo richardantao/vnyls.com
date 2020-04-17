c2244414297a1addde8aa10904f9cf662b704ce0
Richard Antao <richardmantao@gmail.com>
2020-04-17 14:27:35 -0400

    Imported db

---
+++ config/db.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const mongoDB = process.env.DB_URL;
+
+const options = {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    connectTimeoutMS: 10000, 
+    socketTimeoutMS: 45000
+};
+
+mongoose.connect(mongoDB, options)
+.then(() =>  {
+  console.log("Connected to Mongo client");
+})
+.catch(err => {
+  console.error(err);
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error: "));
+db.once("open", () => {
+  console.log("Connecting to database...");
+});
+
+module.exports = db;
