3c03f828ccd664cf531825a0ec7acc638189d0dc
Richard Antao <richardmantao@gmail.com>
2020-04-20 14:18:30 -0400

    Added cookie parser

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const path = require("path");
 const helmet = require("helmet");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const xss = require("xss-clean");
 
 /* --- Configuration --- */
 const app = express();
@@ -13,11 +15,16 @@ require("./config/db");
 /* --- Middleware --- */
 app.use(helmet());
 app.use(cors());
+app.use(cookieParser());
+app.use(xss());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "./views/build")));
 
 /* --- Routes --- */
 app.use("/api", require("./routes/auth"));
+app.use("/api", require("./routes/music"));
+app.use("/api", require("./routes/paypal"));
+app.use("/api", require("./routes/stripe"));
 app.use("/api/errors", require("./routes/errors"));
 
 app.get("*", (req, res) => {
