f26b70f18d3071a13f47866f6262948aec56b100
Richard Antao <richardmantao@gmail.com>
2020-04-22 18:42:38 -0400

    Added songs reducer

---
+++ views/src/reducers/songs.js
@@ -0,0 +1,28 @@
+import { SONGS_REQUESTED, SONGS_ERROR, SONGS_FETCHED } from "../actions/types";
+
+const initialState = {
+    isLoading: false,
+    songs: null
+};
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case SONGS_REQUESTED:
+            return {
+                ...state,
+                isLoading: true
+            };
+        case SONGS_ERROR:
+            return {
+                ...state,
+                isLoading: false
+            };
+        case SONGS_FETCHED:
+            return {
+                isLoading: false,
+                songs: action.payload
+            };
+        default:
+            return state;
+    };
+};
