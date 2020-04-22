ce60c09174cd4908505de8930432823c0597e94b
Richard Antao <richardmantao@gmail.com>
2020-04-22 18:42:46 -0400

    Imported songs into global state

--- views/src/reducers/index.js
+++ views/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import auth from "./auth";
 import error from "./errors";
-
+import song from "./songs";
 
 export default combineReducers({ 
-
+    auth,
+    error,
+    song
 });
