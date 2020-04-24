7f4c7fc200eab29040b520e38d79477c9d40ebaf
Richard Antao <richardmantao@gmail.com>
2020-04-24 18:14:35 -0400

    Imported Provider

--- views/src/index.js
+++ views/src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import "./index.scss";
 import "bootstrap/dist/css/bootstrap.css";
 
 render(
     <Router>
-        <App />
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </Router>,
     document.querySelector("#root")
 );
