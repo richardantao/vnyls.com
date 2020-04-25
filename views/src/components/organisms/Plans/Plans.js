5087ba9e65971e31cf2f04866dbff4dc76d16dba
Richard Antao <richardmantao@gmail.com>
2020-04-25 11:42:59 -0400

    Initialized component

---
+++ views/src/components/organisms/Plans/Plans.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+import { Card } from "reactstrap";
+
+import "./Plans.scss";
+
+export default ({ }) => {
+    const [isMonthly, toggleMonthly] = useState(true);
+
+    return (
+        <Card
+            
+        
+        />
+    );
+};
