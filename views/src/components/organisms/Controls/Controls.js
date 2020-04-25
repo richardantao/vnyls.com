b8de33284e2c1e2db376162b73f7fd6689544688
Richard Antao <richardmantao@gmail.com>
2020-04-25 14:31:56 -0400

    Initialized component

---
+++ views/src/components/organisms/Controls/Controls.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+import { } from "../../../actions/songs";
+import PropTypes from "prop-types";
+
+import "./Controls.scss";
+
+class Controls extends Component {
+    state = {
+
+    };
+
+    static propTypes {
+
+    };
+
+    componentDidUpdate(prevProps) {
+
+    };
+
+    render() {
+        const { } = this.props;
+    };
+};
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = { };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Controls);
