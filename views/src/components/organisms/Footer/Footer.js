4d92105e71b4c2bf8f37747b7f030bff2d7f9e33
Richard Antao <richardmantao@gmail.com>
2020-04-25 12:04:37 -0400

    Fixed Date error

--- views/src/components/organisms/Footer/Footer.js
+++ views/src/components/organisms/Footer/Footer.js
@@ -5,9 +5,9 @@ import "./Footer.scss";
 export default () => {
     return (
         <footer role="content-info" id="footer">
-            <span id="copyright">Copyright © {year} Vynls.com | Design by <a href="https://richardantao.com">richardantao.com</a></span>
-        </footer>   
+            <span id="copyright">Copyright © {year} Vnyls.com | Design by <a href="https://richardantao.com" target="_blank">richardantao.com</a></span>
+        </footer>
     );
 };
 
-const year = new Date.getFullYear();
+const year = new Date().getFullYear();
