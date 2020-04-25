8f78e7031e18855c022839d866dba92bdbc4e6e2
Richard Antao <richardmantao@gmail.com>
2020-04-25 11:42:37 -0400

    Cleaned code

--- views/src/store.js
+++ views/src/store.js
@@ -10,6 +10,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
   rootReducer,
-  initialState, 
+  initialState,
   composeEnhancers(applyMiddleware(...middleware))
 );
