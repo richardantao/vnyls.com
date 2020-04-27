f18aba643d94b855580dbe595b90913e1eaa69f0
Richard Antao <richardmantao@gmail.com>
2020-04-27 17:56:48 -0400

    Specified error actions

--- views/src/actions/auth.js
+++ views/src/actions/auth.js
@@ -1,4 +1,10 @@
-import { USER_REQUESTED, USER_LOADED, AUTH_ERROR, REGISTER_SUCCESS, LOGIN_SUCCESS, LOGOUT_SUCCESS } from "./types";
+import {
+    USER_REQUESTED, USER_LOADED,
+    AUTH_ERROR,
+    REGISTER_SUCCESS, REGISTER_FAILED,
+    LOGIN_SUCCESS, LOGIN_FAILED,
+    LOGOUT_SUCCESS
+} from "./types";
 import { returnErrors } from "./errors";
 import axios from "axios";
 
@@ -24,7 +30,7 @@ export const loadUser = () => (dispatch, getState) => {
     });
 };
 
-export const register = user => dispatch => {    
+export const register = user => dispatch => {
     const config = {
         headers: {
             "Content-Type": "application/json"
@@ -33,20 +39,20 @@ export const register = user => dispatch => {
 
     axios.post("api/v1/users", user, config)
     .then(res => dispatch({
-       type: REGISTER_SUCCESS,
-       payload: res.data 
+        type: REGISTER_SUCCESS,
+        payload: res.data
     }))
     .catch(err => {
         if(err.response) {
             dispatch(returnErrors(err.response.data, err.response.status, "REGISTER_FAILED"));
-            dispatch({ type: AUTH_ERROR });
+            dispatch({ type: REGISTER_FAILED });
         } else if(err.request) {
             dispatch(returnErrors(err.request.data, err.request.status, "REGISTER_FAILED"));
-            dispatch({ type: AUTH_ERROR });
+            dispatch({ type: REGISTER_FAILED });
         };
 
         dispatch(returnErrors(err.request.data, err.request.status, "REGISTER_FAILED"));
-        dispatch({ type: AUTH_ERROR });
+        dispatch({ type: REGISTER_FAILED });
     });
 };
 
@@ -56,7 +62,7 @@ export const login = user => dispatch => {
             "Content-Type": "application/json"
         }
     };
-    
+
     axios.post("/api/v1/users/authentication", user, config)
     .then(res => dispatch({
         type: LOGIN_SUCCESS,
@@ -65,14 +71,14 @@ export const login = user => dispatch => {
     .catch(err => {
         if(err.response) {
             dispatch(returnErrors(err.response.data, err.response.status, "LOGIN_FAILED"));
-            dispatch({ type: AUTH_ERROR });
+            dispatch({ type: LOGIN_FAILED });
         } else if(err.request) {
             dispatch(returnErrors(err.request.data, err.request.status, "LOGIN_FAILED"));
-            dispatch({ type: AUTH_ERROR });
+            dispatch({ type: LOGIN_FAILED });
         };
 
         dispatch(returnErrors(err.request.data, err.request.status, "LOGIN_FAILED"));
-        dispatch({ type: AUTH_ERROR });
+        dispatch({ type: LOGIN_FAILED });
     });
 };
 
@@ -87,15 +93,15 @@ export const logout = () => dispatch => {
 };
 
 export const tokenConfig = getState => {
-    // const token = getState().auth.token;
-    
+    const token = getState().auth.token; // change to get token from cookie
+
     const config = {
         headers: {
-            "Content-Type": "application/json" 
+            "Content-Type": "application/json"
         }
     };
 
-    // if(token) config.headers["x-auth-token"] = token;
-    
+    if(token) config.headers["x-auth-token"] = token;
+
     return config;
 };
