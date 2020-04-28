895b7bc50f220f56e8198e60166f9cdd04002a18
Richard Antao <richardmantao@gmail.com>
2020-04-27 23:09:30 -0400

    Destructured token from cookie

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,15 +3,16 @@ const jwt = require("jsonwebtoken");
 const secret = process.env.AUTH_SECRET;
 
 module.exports = (req, res, next) => {
-    const token = req.header("x-auth-token");
-    
+    const token = req.cookies.token;
+
     if(!token) return res.status(401).json({ message: "Missing token, authorization denied" });
-    
+
     try {
         req.user = jwt.verify(token, secret);
-    
+
         return next();
     } catch (err) {
-        return es.status(400).json({ message: "Token is not valid" });
-    };  
+        res.clearCookie("access_token");
+        return res.status(400).json({ message: "Token is not valid" });
+    };
 };
