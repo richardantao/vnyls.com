6413dc0b95ddcae5d77e884da314616c73e71cf6
Richard Antao <richardmantao@gmail.com>
2020-05-01 23:24:03 -0400

    Changed authentication resource name

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,10 @@ router.post("/users", validation.register, controller.register);
 
 router.get("/users", auth, controller.user);
 
-router.delete("/users", auth, controller.terminate);
+router.delete("/users", auth, controller.deregister);
 
-router.post("/users/authentication", validation.login, controller.login);
+router.post("/users/access-token", validation.login, controller.login);
 
-router.delete("/users/authentication", auth, controller.logout);
+router.delete("/users/access-token", /*auth,*/ controller.logout);
 
 module.exports = router;
