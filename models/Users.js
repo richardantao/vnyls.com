4de5d01bcc23d51261d4daf6612e5358105576c9
Richard Antao <richardmantao@gmail.com>
2020-05-01 23:50:07 -0400

    Removed options

--- models/Users.js
+++ models/Users.js
@@ -3,11 +3,8 @@ const { model, Schema } = require("mongoose");
 module.exports = model("users", new Schema({
     name: {
         first: { type: String, required: true },
-        last: { type: String, required: true } 
+        last: { type: String, required: true }
     },
-    email: { type: String, required: true }, 
-    password: { type: String, required: true } 
-
-}, {
-    timestamps: true
-}))
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+}));
