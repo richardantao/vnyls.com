8b5fef7191c7e252ffcd096c3f6240f8b54af5a1
Richard Antao <richardmantao@gmail.com>
2020-05-01 22:13:59 -0400

    Chained async fetching for entries and links

--- controllers/songs.js
+++ controllers/songs.js
@@ -1,13 +1,44 @@
 const Dropbox = require("../config/dropbox");
+const async = require("async");
 
 module.exports = (req, res) => {
-    Dropbox.filesListFolder({ path: "" })
-    .then(songs => {
-        if(!songs) return res.status(404).json({ message: "Songs not found" });
+    const fetchPaths = callback => {
+        Dropbox.filesListFolder({ path: "" })
+        .then(folder => {
+            return callback(null, folder.entries);
+        })
+        .catch(err => {
+            return res.status(500).json(err.message);
+        });
+    };
 
-        return res.status(200).json(songs);
-    })
-    .catch(err => {
-        return res.status(500).json({ message: err.message });
+    const getLinks = (entries, callback) => {
+        let links = [];
+
+        for(let i = 0; i < entries.length; i++) {
+            const downloadFile = path => {
+                Dropbox.filesGetTemporaryLink({ path })
+                .then(file => {
+                    return appendLinksList(file.link);
+                })
+                .catch(err => {
+                    return res.status(500).json(err.message);
+                });
+            };
+
+            downloadFile(entries[i].path_display);
+
+            const appendLinksList = link => {
+                if(i !== entries.length - 1) return links = [...links, link];
+
+                return callback(null, links);
+            };
+        };
+    };
+
+    async.waterfall([ fetchPaths, getLinks ], (err, results) => {
+        if(err) return res.status(500).json({ message: err.message });
+
+        return res.status(200).json(results);
     });
 };
