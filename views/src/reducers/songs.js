ae6268b80a1aded2923489d15e486f3815f17e96
Richard Antao <richardmantao@gmail.com>
2020-05-01 23:54:11 -0400

    Changed intial state of songs to an empty array

--- views/src/reducers/songs.js
+++ views/src/reducers/songs.js
@@ -2,7 +2,7 @@ import { SONGS_REQUESTED, SONGS_ERROR, SONGS_FETCHED } from "../actions/types";
 
 const initialState = {
     isLoading: false,
-    songs: null
+    songs: []
 };
 
 export default (state = initialState, action) => {
@@ -19,6 +19,7 @@ export default (state = initialState, action) => {
             };
         case SONGS_FETCHED:
             return {
+                ...state,
                 isLoading: false,
                 songs: action.payload
             };
