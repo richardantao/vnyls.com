14f393e2a438a8d56de3358070665e323c98e188
Richard Antao <richardmantao@gmail.com>
2020-05-02 21:53:07 -0400

    Added payment IDs

--- models/Users.js
+++ models/Users.js
@@ -1,10 +1,12 @@
 const { model, Schema } = require("mongoose");
 
 module.exports = model("users", new Schema({
+    paypal: Schema.Types.ObjectId,
+    stripe: Schema.Types.ObjectId,
     name: {
         first: { type: String, required: true },
         last: { type: String, required: true }
     },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
 }));
