fabcaf162b4b23b64e12021d786d5c153b8fbc9a
Richard Antao <richardmantao@gmail.com>
2020-05-03 23:46:10 -0400

    Added name and phone validation

--- middleware/validation/stripe.js
+++ middleware/validation/stripe.js
@@ -3,12 +3,22 @@ const { body, validationResult } = require("express-validator");
 module.exports = (req, res, next) => {
     const errors = validationResult(req);
 
+    body("name", "Name received an invalid input")
+    .exists().withMessage("Name is a required field")
+    .trim()
+    .escape();
+
     body("email", "Email received an invalid input")
     .exists().withMessage("Email is a required field")
     .isEmail().withMessage("Email must be a valid email address")
     .trim()
     .escape()
     .normalizeEmail();
+
+    body("phone", "Phone received an invalid input")
+    .exists().withMessage("Phone is a required field")
+    .trim()
+    .escape();
 
     if(!errors.isEmpty()) return res.status(400).json(errors.msg);
 
