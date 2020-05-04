4ec5382cbb2a8c6d11c59939f1aa60ab9650a343
Richard Antao <richardmantao@gmail.com>
2020-05-03 22:51:27 -0400

    Added email validation

--- routes/paypal.js
+++ routes/paypal.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
+const validation = require("../middleware/validation/paypal");
 const controller = require("../controllers/paypal");
 
-router.post("/paypal/subscriptions", controller.create);
+router.post("/paypal/subscriptions", validation, controller.create);
 
 router.get("/paypal/subscription/:_id", controller.return);
 
-router.put("/paypal/subscription/:_id", controller.update);
+router.put("/paypal/subscription/:_id", validation, controller.update);
 
 router.delete("/paypal/subscriptions/:_id", controller.cancel);
 
