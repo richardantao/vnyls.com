4e5fb878ba0dc2421c67efdbe41f0029f2f5ba10
Richard Antao <richardmantao@gmail.com>
2020-05-04 01:27:05 -0400

    Updated tests

--- test/integration/auth.test.js
+++ test/integration/auth.test.js
@@ -9,7 +9,7 @@ const User = require("../../models/Users");
 
 const bcrypt = require("bcryptjs");
 
-const { register } = require("../data/auth.json");
+const { register, login } = require("../data/auth.json");
 
 chai.use(chaiHttp);
 
@@ -30,57 +30,72 @@ describe("POST /api/users", () => {
         .send(register.good)
         .end((err, res) => {
             if(err) throw err;
-           
+
             res.should.have.property("status", 201);
+
             res.body.should.be.a("object");
 
+            res.body.should.have.property("name");
+            res.body.should.have.property("email");
+            res.body.should.have.property("password");
+
+            res.body.name.should.be.a("object");
+            res.body.email.should.be.a("string");
+            res.body.password.should.be.a("string");
+
+            res.body.name.first.should.be.a("string");
+            res.body.name.last.should.be.a("string");
+
             done();
         });
     });
 
-    it("It should return a 500 status from the model with the error message when a field is left blank", done => {
-        chai.request(server)
-        .post("/api/users")
-        .send(register.missing)
-        .end((err, res) => {
-            if(err) throw err;
+//     it("It should return a 500 status from the model with the error message when a field is left blank", done => {
+//         chai.request(server)
+//         .post("/api/users")
+//         .send(register.missing)
+//         .end((err, res) => {
+//             if(err) throw err;
            
-            res.should.have.property("status", 500);
-            res.body.should.have.property("message");
+//             res.should.have.property("status", 500);
 
-            done();
-        });
-    });
+//             res.body.should.have.property("message");
+            
+//             done();
+//         });
+//     });
 
-    it("It should return a 400 status when there is an invalid email", done => {
-        chai.request(server)
-        .post("/api/users")
-        .send(register.invalidEmail)
-        .end((err, res) => {
-            if(err) throw err;
+//     it("It should return a 400 status when there is an invalid email", done => {
+//         chai.request(server)
+//         .post("/api/users")
+//         .send(register.invalidEmail)
+//         .end((err, res) => {
+//             if(err) throw err;
 
-            // console.log(res);
+//             // console.log(res);
            
-            res.should.have.property("status", 400);
-            res.body.should.have.property("message");
+//             res.should.have.property("status", 400);
 
-            done();
-        });
-    });
+//             res.body.should.have.property("message");
 
-    it("It should return a 400 status with the error message when the user's password is too short", done => {
-        chai.request(server)
-        .post("/api/users")
-        .send(register.shortPassword)
-        .end((err, res) => {
-            if(err) throw err;
+//             done();
+//         });
+//     });
+
+//     it("It should return a 400 status with the error message when the user's password is too short", done => {
+//         chai.request(server)
+//         .post("/api/users")
+//         .send(register.shortPassword)
+//         .end((err, res) => {
+//             if(err) throw err;
            
-            res.should.have.property("status", 400);
-            res.body.should.have.property("message");
+//             res.should.have.property("status", 400);
 
-            done();
-        });
-    });
+//             res.body.should.have.property("message");
+
+//             done();
+//         });
+//     });
 });
 
 // describe("GET /api/users", () => {
@@ -94,40 +109,55 @@ describe("POST /api/users", () => {
 //         });
 //     });
 
-//     it("", done => {
+//     it("It should return a 200 status with the user object", done => {
 //         chai.request(server)
 //         .get("/api/users")
 //         .end((err, res) => {
 //             if(err) throw err;
            
 //             res.status.should.be(200);
+    
 //             res.body.should.be.a("object");
+        
+//             res.body.should.have.property("name");
+//             res.body.should.have.property("email");
+
+//             res.body.name.should.be.a("object");
+//             res.body.name.should.have.property("first");
+//             res.body.name.should.have.property("last");
+
+//             res.body.name.first.should.be.a("string");
+//             res.body.name.last.should.be.a("string");
+
+//             res.body.email.should.be.a("string");
 
 //             done();
 //         });
 //     });
 
-//     it("", done => {
+//     it("It should return a 404 status if the user object could not be retrieved", done => {
 //         chai.request(server)
 //         .get("/api/users")
 //         .end((err, res) => {
 //             if(err) throw err;
            
-//             res.status.should.be();
-//             res.body.should.be.a("object");
+//             res.should.have.property("status", 404);
+
+//             res.body.should.have.property("message", "User information could not be found");
 
 //             done();
 //         });
 //     });
 
-//     it("", done => {
+//     it("It should return a 401 status if the request is not successfully authenticated", done => {
 //         chai.request(server)
 //         .get("/api/users")
 //         .end((err, res) => {
 //             if(err) throw err;
            
-//             res.status.should.be();
-//             res.body.should.be.a("object");
+//             res.should.have.property("status", 401);
+
+//             res.body.should.have.property("message");
 
 //             done();
 //         });
@@ -135,109 +165,114 @@ describe("POST /api/users", () => {
 // });
 
 // describe("DELETE /api/users", () => {
-//     it("", done => {
+//     it("It should return a 200 status code when the user's account is successfully deleted", done => {
 //         chai.request(server)
 //         .delete("/api/users")
 //         .end((err, res) => {
 //             if(err) throw err;
         
-//             res.status.should.be(200);
+//             res.should.have.property("status", 200);
 
 //             done();
 //         });
 //     });
 
-//     it("", done => {
+//     it("It should return a 404 status if the user's account could not be found", done => {
 //         chai.request(server)
 //         .delete("/api/users")
 //         .end((err, res) => {
 //             if(err) throw err;
         
-//             res.status.should.be();
+//             res.should.have.property("status", 404);
+
+//             res.body.should.have.property("message");
 
 //             done();
 //         });
 //     });
 
-//     it("", done => {
+//     it("It should return a 401 status if the request is not successfully authenticated", done => {
 //         chai.request(server)
 //         .delete("/api/users")
 //         .end((err, res) => {
 //             if(err) throw err;
         
-//             res.status.should.be();
+//             res.should.have.property("status", 401);
+
+//             res.body.should.have.property("message");
 
 //             done();
 //         });
 //     });
 // });
 
-describe("POST /api/users/authentication", () => {
-    beforeEach(done => {
-        bcrypt.genSalt(10, (err, salt) => {        
-            bcrypt.hash(register.good.password, salt, (err, hash) => {            
-                User.create({
-                    name: {
-                        first: register.good.first,
-                        last: register.good.last
-                    },
-                    email: register.good.email,
-                    password: hash
-                });
-            });
-        });
+describe("POST /api/users/access-token", () => {
+    // beforeEach(done => {
+    //     bcrypt.genSalt(10, (err, salt) => {        
+    //         bcrypt.hash(register.good.password, salt, (err, hash) => {            
+    //             User.create({
+    //                 name: {
+    //                     first: register.good.first,
+    //                     last: register.good.last
+    //                 },
+    //                 email: register.good.email,
+    //                 password: hash
+    //             });
+    //         });
+    //     });
 
-        done();
-    });
+    //     done();
+    // });
 
-    it("It should return a 201 status and the cookie object", done => {
+    it("It should return a 201 status and the success message", done => {
         chai.request(server)
-        .post("/api/users/authentication")
-        .send({ 
-            email: register.good.email,
-            password: register.good.password
+        .post("/api/users/access-token")
+        .send({
+            email: login.good.email,
+            password: login.good.password
         })
         .end((err, res) => {
             if(err) throw err;
 
-            console.log(res);
-           
             res.should.have.property("status", 201);
-            res.body.should.have.property("cookie");
+
+            res.body.should.be.a("string");
+
+            res.body.should.equal("Login successful");
 
             done();
         });
     });
 
-    it("It should return 404 status if the email isn't already registered", done => {
-        chai.request(server)
-        .post("/api/users/authentication")
-        .end((err, res) => {
-            if(err) throw err;
+    // it("It should return 404 status if the email isn't already registered", done => {
+    //     chai.request(server)
+    //     .post("/api/users/access-token")
+    //     .end((err, res) => {
+    //         if(err) throw err;
            
-            res.should.have.property("status", 404);
+    //         res.should.have.property("status", 404);
 
-            done();
-        });
-    });
+    //         done();
+    //     });
+    // });
 
-    it("It should return a 400 status if the password is incorrect", done => {
-        chai.request(server)
-        .post("/api/users/authentication")
-        .end((err, res) => {
-            if(err) throw err;
+    // it("It should return a 400 status if the password is incorrect", done => {
+    //     chai.request(server)
+    //     .post("/api/users/access-token")
+    //     .end((err, res) => {
+    //         if(err) throw err;
            
-            res.status.should.be();
+    //         res.should.have.property("status", )
 
-            done();
-        });
-    });
+    //         done();
+    //     });
+    // });
 });
 
-// describe("DELETE /api/users/authentication", () => {
+// describe("DELETE /api/users/access-token", () => {
 //     it("It should return a 200 status code", done => {
 //         chai.request(server)
-//         .delete("/api/users/authentication")
+//         .delete("/api/users/access-token")
 //         .end((err, res) => {
 //             if(err) throw err;
            
@@ -249,7 +284,7 @@ describe("POST /api/users/authentication", () => {
 
 //     it("", done => {
 //         chai.request(server)
-//         .delete("/api/users/authentication")
+//         .delete("/api/users/access-token")
 //         .end((err, res) => {
 //             if(err) throw err;
            
@@ -261,7 +296,7 @@ describe("POST /api/users/authentication", () => {
 
 //     it("", done => {
 //         chai.request(server)
-//         .delete("/api/users/authentication")
+//         .delete("/api/users/access-token")
 //         .end((err, res) => {
 //             if(err) throw err;
            
